735881361d10232e6963bef5a87886cddb010bf4
Shin, SJ <lannex.shin@gmail.com>
2023-09-06 13:29:39 +0900

    refactor: cleanup

--- .config/nvim/lua/custom/configs/null-ls.lua
+++ .config/nvim/lua/custom/configs/null-ls.lua
@@ -3,13 +3,12 @@ local null_ls = require("null-ls")
 local b = null_ls.builtins
 
 local sources = {
-
-	-- webdev stuff
 	b.formatting.deno_fmt, -- choosed deno for ts/js files cuz its very fast!
 	b.formatting.prettier.with({ filetypes = { "html", "markdown", "css" } }), -- so prettier works only on these filetypes
 
-	-- cpp
 	b.formatting.clang_format,
+
+	b.formatting.taplo,
 }
 
 null_ls.setup({
