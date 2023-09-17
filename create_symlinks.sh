b6dadd7b26f6b727f0ec5f7b06507c201ffbf242
Shin, SJ <lannex.shin@gmail.com>
2023-09-17 15:20:57 +0900

    chore: update symlink script

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -8,6 +8,6 @@ TARGET_DIR=~/.config
 
 # 대상 파일 목록을 순회하며 심볼릭 링크 생성
 for item in "${SOURCES[@]}"; do
-  ln -s "$SOURCE_DIR/.config/$item" "$TARGET_DIR/$item"
+  ln -fs "$SOURCE_DIR/.config/$item" "$TARGET_DIR/$item"
 done
 
