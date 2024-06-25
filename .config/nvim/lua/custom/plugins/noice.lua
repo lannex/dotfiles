bb36e1db6c0867ed27ab74a36ecfec4142e69fdb
Shin, SJ <lannex.shin@gmail.com>
2024-06-25 16:33:54 +0900

    chore: update noice settings

--- .config/nvim/lua/custom/plugins/noice.lua
+++ .config/nvim/lua/custom/plugins/noice.lua
@@ -13,11 +13,27 @@ return {
 				},
 			},
 			presets = {
-				bottom_search = true, -- use a classic bottom cmdline for search
+				bottom_search = false, -- use a classic bottom cmdline for search
 				command_palette = true, -- position the cmdline and popupmenu together
 				long_message_to_split = true, -- long messages will be sent to a split
 				inc_rename = false, -- enables an input dialog for inc-rename.nvim
 				lsp_doc_border = false, -- add a border to hover docs and signature help
+			},
+			views = {
+				cmdline_popup = {
+					position = {
+						row = 3,
+						col = "50%",
+					},
+					size = {
+						width = 60,
+						height = "auto",
+					},
+					border = {
+						style = "rounded",
+						padding = { 0, 1 },
+					},
+				},
 			},
 		})
 	end,
