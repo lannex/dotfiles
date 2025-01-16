f2a74e73a5445cb855528ed4cff66662588a1455
Shin, SJ <lannex.shin@gmail.com>
2025-01-16 14:13:55 +0900

    feat: Add "ghostty" cask and config file to project
    - Add `ghostty` cask to `Brewfile` in homebrew directory
    - Update `create_symlinks.sh` to include "ghostty" in the `config_list` array

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -4,7 +4,7 @@ source_dir=~/dotfiles
 
 create_config_symlinks() {
   local target_dir=~/.config
-  local config_list=(nvim/lua/custom wezterm zed zellij)
+  local config_list=(nvim/lua/custom wezterm zed zellij ghostty)
 
   for item in "${config_list[@]}"; do
     rm -r "$target_dir/$item"
